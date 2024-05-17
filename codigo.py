3d123163704e173038b9acdab14875dd2dee5fed
Brenner Yubiny Granados Pinto <brennerygp@hotmail.com>
2024-05-16 20:08:34 -0600

    Primer Commit

---
+++ codigo.py
@@ -0,0 +1,9 @@
+nombre = "Brenner"
+apellido = "Granados"
+
+print("Hola" + nombre + " " + apellido + " ¿Como Estas?" )nombre = "Brenner"
+apellido = "Granados"
+
+saludo = "Hola" + nombre + " " + apellido
+
+print (saludo)
